933bd47db3382b502210e821a6f0fae3c358040e
ishanajmeri <ajmeriishansurat@gmail.com>
2020-03-22 23:25:12 +0530

    trying to add AI and 2 player name in home

--- modules/upload/client-react/new/home.jsx
+++ modules/upload/client-react/new/home.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { withFormik, Form } from 'formik';
-import { RenderField, LayoutCenter, Button } from '@gqlapp/look-client-react';
-import { FieldAdapter as Field } from '@gqlapp/forms-client-react';
+import { withFormik } from 'formik';
+// import { Form } from 'formik';
+// import { RenderField, LayoutCenter, Button } from '@gqlapp/look-client-react';
+import { LayoutCenter } from '@gqlapp/look-client-react';
+// import { FieldAdapter as Field } from '@gqlapp/forms-client-react';
 
 import { minLength, required, validate } from '@gqlapp/validation-common-react';
 import { PropTypes } from 'prop-types';
+// import { DownOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
 
 const formsFormSchema = {
   player1: [required, minLength(4)],
@@ -13,18 +17,44 @@ const formsFormSchema = {
 
 class App extends Component {
   render() {
-    const { values, handleSubmit, errors } = this.props;
+    const { SubMenu } = Menu;
+    // const { values, handleSubmit, errors } = this.props;
     return (
       <LayoutCenter>
-        <h2 className="text-center">Name of Players </h2>
+        <Menu style={{ width: 256 }} mode="inline">
+          <SubMenu key="sub1" title={<span>With AI</span>}>
+            <Menu.Item key="1">name</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" title={<span>2 players</span>}>
+            <Menu.Item key="2">name1</Menu.Item>
+            <Menu.Item key="3">name2</Menu.Item>
+          </SubMenu>
+        </Menu>
+        {/* <h2 className="text-center">Name of Players </h2>
         <Form onSubmit={handleSubmit}>
-          <Field name="player1" component={RenderField} type="text" value={values.player1} label="Player1" />
-          <Field name="player2" component={RenderField} type="text" value={values.player2} label="Player2" />
+          <Field
+            name="player1"
+            component={RenderField}
+            type="text"
+            value={values.player1}
+            label="Player1"
+          />
+          <Field
+            name="player2"
+            component={RenderField}
+            type="text"
+            value={values.player2}
+            label="Player2"
+          />
 
-          <Button color="primary" type="submit" disabled={errors.player1 || errors.player2}>
+          <Button
+            color="primary"
+            type="submit"
+            disabled={errors.player1 || errors.player2}
+          >
             Submit
           </Button>
-        </Form>
+        </Form> */}
       </LayoutCenter>
     );
   }
