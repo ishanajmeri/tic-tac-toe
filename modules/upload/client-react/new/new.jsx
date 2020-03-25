f11bcd0ed16992755ae89ab98707a7cfc66754eb
ishanajmeri <ajmeriishansurat@gmail.com>
2020-03-25 16:40:49 +0530

    adding table in AI page

--- modules/upload/client-react/new/new.jsx
+++ modules/upload/client-react/new/new.jsx
@@ -1,9 +1,69 @@
 import React, { Component } from 'react';
+import { Button, Row, Col } from 'antd';
 
 class New extends Component {
-  state = {};
+  state = {
+    data: Array(9).fill(null),
+    element: true,
+    tie: 0,
+    player: 'x'
+  };
+
+  handleClick = int => {
+    let newdata = this.state.data;
+
+    newdata[int] = this.state.player;
+    this.setState({ data: newdata, player: this.state.player === 'x' ? 'o' : 'x' });
+  };
+
   render() {
-    return <h1>new</h1>;
+    const { data } = this.state;
+    console.log(data);
+    const Td = ({ int, value }) => {
+      return (
+        <td style={{ cursor: 'pointer', height: 48, width: 40 }} onClick={() => this.handleClick(int)}>
+          {value}
+        </td>
+      );
+    };
+    return (
+      <React.Fragment>
+        <Row type="flex" justify="center">
+          <h2>TIC TAC TOE</h2>
+        </Row>
+        <Row type="flex" justify="center">
+          <Col>
+            <Button type="primary">Restart</Button>
+          </Col>
+        </Row>
+        {/* <Row type="flex" justify="center">
+          status
+          </Row> */}
+        <Row type="flex" justify="center" style={{ padding: '5px' }}>
+          <Col>
+            <table className="table table-bordered">
+              <tbody>
+                <tr>
+                  <Td int="0" value={data[0]} />
+                  <Td int="1" value={data[1]} />
+                  <Td int="2" value={data[2]} />
+                </tr>
+                <tr>
+                  <Td int="3" value={data[3]} />
+                  <Td int="4" value={data[4]} />
+                  <Td int="5" value={data[5]} />
+                </tr>
+                <tr>
+                  <Td int="6" value={data[6]} />
+                  <Td int="7" value={data[7]} />
+                  <Td int="8" value={data[8]} />
+                </tr>
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+      </React.Fragment>
+    );
   }
 }
 
--- modules/upload/client-react/new/tic-tac-toe.jsx
+++ modules/upload/client-react/new/tic-tac-toe.jsx
@@ -11,17 +11,7 @@ class Game extends Component {
 
   isWinner = data => {
     // let temp = null;
-    // eslint-disable-next-line prettier/prettier
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (data[a] === data[b] && data[a] === data[c]) return data[a];
@@ -48,11 +38,7 @@ class Game extends Component {
   render() {
     const Td = ({ int, value }) => {
       return (
-        // eslint-disable-next-line prettier/prettier
-        <td
-          style={{ cursor: 'pointer', height: 48, width: 40 }}
-          onClick={() => this.handleClick(int)}
-        >
+        <td style={{ cursor: 'pointer', height: 48, width: 40 }} onClick={() => this.handleClick(int)}>
           {value}
         </td>
       );
