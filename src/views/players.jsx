ba38db4280818ab33b1568f6de4c888727d905b6
ishanajmeri <ajmeriishansurat@gmail.com>
2020-07-05 15:37:33 +0530

    final

--- src/views/players.jsx
+++ src/views/players.jsx
@@ -82,63 +82,77 @@ class Players extends Component {
     return (
       this.props.location.values !== undefined && (
         <React.Fragment>
-          <Row type="flex" justify="center">
-            <h2>TIC TAC TOE</h2>
-          </Row>
-          <Row type="flex" justify="center">
-            <Col>
-              <Button type="primary" onClick={this.handleRestart}>
-                Restart
-              </Button>
-            </Col>
-          </Row>
-          {(status === `${values.player1}'s turn` ||
-            status === `${values.player2}'s turn`) && (
+          <div
+            style={{
+              height: '100vh',
+            }}
+          >
+            <Row type="flex" justify="center" style={{ paddingTop: '4%' }}>
+              <h1
+                style={{
+                  fontFamily: "'Times New Roman', Times, serif",
+                  fontSize: '44px',
+                }}
+              >
+                TIC-TAC-TOE
+              </h1>
+            </Row>
             <Row type="flex" justify="center">
               <Col>
-                <div
-                  style={{
-                    padding: '10px',
-                    alignSelf: 'center',
-                    fontSize: '24px',
-                  }}
-                >
-                  {status}
-                </div>
+                <Button type="primary" onClick={this.handleRestart}>
+                  Restart
+                </Button>
               </Col>
             </Row>
-          )}
-          <Row type="flex" justify="center" style={{ padding: '5px' }}>
-            <Col>
-              <table style={{ border: '1px solid black' }}>
-                <tbody>
-                  <tr style={{ border: '1px solid black' }}>
-                    <Td int="0" value={data[0]} />
-                    <Td int="1" value={data[1]} />
-                    <Td int="2" value={data[2]} />
-                  </tr>
-                  <tr style={{ border: '1px solid black' }}>
-                    <Td int="3" value={data[3]} />
-                    <Td int="4" value={data[4]} />
-                    <Td int="5" value={data[5]} />
-                  </tr>
-                  <tr style={{ border: '1px solid black' }}>
-                    <Td int="6" value={data[6]} />
-                    <Td int="7" value={data[7]} />
-                    <Td int="8" value={data[8]} />
-                  </tr>
-                </tbody>
-              </table>
-            </Col>
-          </Row>
-          {(status === `winner : ${values.player1}` ||
-            status === `winner : ${values.player2}`) && (
+            {(status === `${values.player1}'s turn` ||
+              status === `${values.player2}'s turn`) && (
+              <Row type="flex" justify="center">
+                <Col>
+                  <h3
+                    style={{
+                      padding: '10px',
+                      alignSelf: 'center',
+                      fontFamily: "'Times New Roman', Times, serif",
+                      fontSize: '24px',
+                    }}
+                  >
+                    {status}
+                  </h3>
+                </Col>
+              </Row>
+            )}
             <Row type="flex" justify="center" style={{ padding: '5px' }}>
-              <h2>
-                <Alert message={status} />
-              </h2>
+              <Col>
+                <table style={{ border: '1px solid black' }}>
+                  <tbody>
+                    <tr style={{ border: '1px solid black' }}>
+                      <Td int="0" value={data[0]} />
+                      <Td int="1" value={data[1]} />
+                      <Td int="2" value={data[2]} />
+                    </tr>
+                    <tr style={{ border: '1px solid black' }}>
+                      <Td int="3" value={data[3]} />
+                      <Td int="4" value={data[4]} />
+                      <Td int="5" value={data[5]} />
+                    </tr>
+                    <tr style={{ border: '1px solid black' }}>
+                      <Td int="6" value={data[6]} />
+                      <Td int="7" value={data[7]} />
+                      <Td int="8" value={data[8]} />
+                    </tr>
+                  </tbody>
+                </table>
+              </Col>
             </Row>
-          )}
+            {(status === `winner : ${values.player1}` ||
+              status === `winner : ${values.player2}`) && (
+              <Row type="flex" justify="center" style={{ padding: '5px' }}>
+                <h2>
+                  <Alert message={status} />
+                </h2>
+              </Row>
+            )}
+          </div>
         </React.Fragment>
       )
     );
